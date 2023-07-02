8819c1531bdea0e5617fe73c184da62a1c95e3fa
Oguntunde Caleb <usegong@gmail.com>
2023-07-02 02:30:40 +0100

    ...

--- tests/socket.test.ts
+++ tests/socket.test.ts
@@ -1,30 +1,15 @@
 import { createClient, Jira, subscribe } from "../src/main";
 
 const client = createClient("http://127.0.0.1:8000", {
-  auth: { token: "0a54470704ada9eac680ea18d4b5a09247f6b352" },
+  auth: { token: "84d58813f131135475729b1f83f1802c9bfc6b39" },
 });
 
 client.on("connect", () => {
-  // jira.get("users", { action: "list" }).then(console.log);
-  // jira.get("users", { action: "retrieve", query: { pk: 1 } }).then(console.log);
-
-  // subscribe("users", { action: "subscribe_user" }, console.log);
-  // jira
-  //   .patch("users", {
-  //     action: "update",
-  //     query: { pk: 1 },
-  //     data: { email: "admin@gmail.com" },
-  //   })
-  //   .then(console.log);
-
-  // jira
-  //   .post("users", {
-  //     action: "create",
-  //     data: { username: Math.random(), password: "1234567" },
-  //   })
-  //   .then(console.dir);
-  jira.delete("users", { action: "destroy", query: { pk: 7 }}).then(console.log)
+  subscribe(
+    "orders",
+    { action: "subscribe", query: { role: "BUSINESS" } },
+    console.log
+  );
 });
-
 
 const jira = new Jira();
